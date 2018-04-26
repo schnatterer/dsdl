51b49b0327a2242c0be6e87701d7cd5ad41bcbd4
schnatterer <johannes@schnatterer.info>
2018-04-26 16:42:18 +0200

    Prepares next dev iteration

--- src/pstd.js
+++ src/pstd.js
@@ -44,7 +44,7 @@ function cli() {
     let urlVal = '';
 
     program
-        .version('0.1.1', '-v, --version')
+        .version('0.1.2-SNAPSHOT', '-v, --version')
         .arguments('<url>').action(function (url) {
         urlVal = url
     })

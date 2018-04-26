b5ff8f65a375b885a747f67815263ca5e6603fd7
schnatterer <johannes@schnatterer.info>
2018-04-26 13:42:14 +0200

    Makes dockerfile buildable on docker hub

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,12 @@
+FROM node:9.5.0-alpine as build
+
+ADD . /
+RUN apk add --update yarn \
+    && yarn install
+
 FROM node:9.5.0-alpine
 
 ADD src/pstd.js /
-ADD node_modules /node_modules
+COPY --from=build /node_modules /node_modules
 
 ENTRYPOINT ["node", "/pstd.js"]
--- createBinaries.js
+++ createBinaries.js
@@ -4,6 +4,10 @@ const {compile} = require('nexe');
 let nodeVersion = '9.5.0';
 let platforms = ['linux-x64', 'windows-x64', 'mac-x64'];
 
+if (process.argv.length > 2) {
+    platforms = [ process.argv[2] ]
+}
+
 platforms.forEach(platform => {
     compile({
         target: `${platform}-${nodeVersion}`,

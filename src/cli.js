8c345505ac84547c6e46e960667029365c4fa8da
schnatterer <johannes@schnatterer.info>
2018-05-18 16:42:08 +0200

    Adds a really simple CLI test

--- src/cli.js
+++ src/cli.js
@@ -5,22 +5,21 @@ const startDate = new Date();
 const Downloader = require('./downloader.js');
 
 function cli() {
-    let urlVal = '';
 
     program
         .version('0.1.2-SNAPSHOT', '-v, --version')
-        .arguments('<url>').action(function (url) { urlVal = url })
+        .arguments('<url>').action(function (url) { program.url = url })
         .option('-u, --user <required>', 'Server user')
         .option('-o, --output <required>', 'Write to this folder')
         .option('-f, --flat [value]', 'Writes all photos to a single folders. If not set, creates subdirs for each tag', false)
         .parse(process.argv);
 
-    if (!program.user || !program.output || !urlVal) {
+    if (!program.user || !program.output || !program.url) {
         program.help()
     }
 
     read({prompt: 'Password: ', silent: true, terminal: true}, function (er, password) {
-        const downloader = new Downloader(urlVal, program);
+        const downloader = new Downloader(program.url, program);
 
         downloader.downloadAllPhotos(password)
             .then(() => {
